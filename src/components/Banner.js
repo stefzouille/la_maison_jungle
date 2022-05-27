92d6520ec4b1adc15dd3af7a9b46e8d1b94dc1ab
stefzouille <contact.lokcy@gmail.com>
2022-05-27 18:03:35 +0200

    add compoonent

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+// function Banner() {
+//   const title = "La maison jungle"
+//   return (<h1>{title}</h1>)
+// }
+
+
+function Banner() {
+  return <h1>La maison jungle</h1>
+}
+
+
+
+
+export default Banner
