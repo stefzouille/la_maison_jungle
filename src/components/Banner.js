84f5941c9db5d1467806d230f2eda9f600157651
stefzouille <contact.lokcy@gmail.com>
2022-05-30 11:47:14 +0200

    add classname & styles

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,28 @@
 // }
 
 
+// import '../styles/Banner.css'
+// import logo from '../assets/logo.png'
+
+// function Banner() {
+//   return <div className='lmj-banner'>
+//     <h1>La maison jungle</h1>
+//   </div>
+// }
+// export default Banner
+
+
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
 function Banner() {
-  return <h1>La maison jungle</h1>
+  const title = 'La maison jungle'
+  return (
+    <div className='lmj-banner'>
+      <img src={logo} alt='La maison jungle' className='lmj-logo' />
+      <h1 className='lmj-title'>{title}</h1>
+    </div>
+  )
 }
-
-
-
 
 export default Banner
