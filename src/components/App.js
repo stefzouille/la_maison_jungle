4b5a0f788cdc7f2ee6bb8c0595dfbc2a5efc0960
stefzouille <contact.lokcy@gmail.com>
2022-06-01 10:37:54 +0200

    add ms prob des cours

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import Banner from './Banner'
 import Cart from './Cart'
 import '../App.css';
+import '../components/ShoppingList'
+import ShoppingList from '../components/ShoppingList';
 
 function App() {
   return (
     <div>
       <Banner />
       <Cart />
+      <ShoppingList />
     </div>
   )
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,47 @@
-const plantList = [
-  'monstera',
-  'ficus lyrata',
-  'pothos argenté',
-  'yucca',
-  'palmier'
-]
+// const plantList = [
+//   'monstera',
+//   'ficus lyrata',
+//   'pothos argenté',
+//   'yucca',
+//   'palmier'
+// ]
 
+
+// function ShoppingList() {
+//   return (
+//     <ul>
+//       {plantList.map((plant, index) => (
+//         <li key={`${plant}-${index}`}>{plant}</li>
+//       ))}
+//     </ul>
+//   )
+// }
+
+// export default ShoppingList
+import { plantList } from '../datas/plantList'
 
 function ShoppingList() {
+  const categories = plantList.reduce(
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    []
+  )
+
   return (
-    <ul>
-      {plantList.map((plant) => (
-        <li>{plant}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {categories.map((cat) => (
+          <li key={cat}>{cat}</li>
+        ))}
+      </ul>
+      <ul>
+        {plantList.map((plant) => (
+          <li key={plant.id}>{plant.name} {plant.isBestSale && <span>🔥</span>}
+          </li>
+        ))}
+
+      </ul>
+    </div>
   )
 }
 
